a2d6701ae324de3cef9d5890e20be100530665a5
Ashtonn77 <ashtonrbay@gmail.com>
2020-05-27 19:13:07 +0200

    added load from local storage

--- index.js
+++ index.js
@@ -4,7 +4,15 @@ const count = document.getElementById("count");
 const total = document.getElementById("total");
 
 const movieSelect = document.getElementById("movie");
+populateUI();
+
 let ticketPrice = +movieSelect.value; /*same as parseInt(movieSelect.value)*/
+
+//save selected movie index and price
+function setMovieData(movieIndex, moviePrice) {
+  localStorage.setItem("selectedMovieIndex", movieIndex);
+  localStorage.setItem("selectedMoviePrice", moviePrice);
+}
 
 function updateSelectedCount() {
   const selectedSeats = document.querySelectorAll(".row .seat.selected");
@@ -12,14 +20,36 @@ function updateSelectedCount() {
   //getiing an array of indexes for seat selection to save locally
   const seatsIndex = [...selectedSeats].map((seat) => [...seats].indexOf(seat));
 
+  //saving selected seats in local storage so user wont lose selection on reload
+  localStorage.setItem("selectedSeats", JSON.stringify(seatsIndex));
+
   const selectedSeatsCount = selectedSeats.length;
   count.innerText = selectedSeatsCount;
   total.innerText = selectedSeatsCount * ticketPrice;
 }
 
+//get data from local storage and populate UI
+function populateUI() {
+  const selectedSeats = JSON.parse(localStorage.getItem("selectedSeats"));
+
+  if (selectedSeats !== null && selectedSeats.length > 0) {
+    seats.forEach((seat, index) => {
+      if (selectedSeats.indexOf(index) > -1) {
+        seat.classList.add("selected");
+      }
+    });
+  }
+
+  const selectedMovieIndex = localStorage.getItem("selectedMovieIndex");
+  if (selectedMovieIndex !== null) {
+    movieSelect.selectedIndex = selectedMovieIndex;
+  }
+}
+
 //change movie event
 movieSelect.addEventListener("change", (e) => {
   ticketPrice = +e.target.value;
+  setMovieData(e.target.selectedIndex, e.target.value);
   updateSelectedCount();
 });
 
@@ -33,3 +63,6 @@ container.addEventListener("click", (e) => {
     updateSelectedCount();
   }
 });
+
+//initial count and total set
+updateSelectedCount();
